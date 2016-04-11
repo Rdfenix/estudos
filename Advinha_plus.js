437156177511cf01c9ace6dd2426b90ffeec8e39
Rudnei Oliveira <rudnei.oliveira@i-value.int>
2016-04-11 16:26:26 -0300

    alteraçoes feitas: adicionado função para realizar o calculo randominco de numero e a condição "IF"

--- Advinha_plus.js
+++ Advinha_plus.js
@@ -1,10 +1,22 @@
 var caixaNumero;
 var botaoAdvinhar;
+var numero;
+var numeroPensado;
 
-function botaoClicado() {
-    alert(caixaNumero.value);
+function randomNumber(x) {
+    var y = Math.random(x) * 100;
+    return y;
 }
 
+function botaoClicado() {
+    if (numeroPensado == caixaNumero.value) {
+        alert("Parabéns! Você acertou o número secreto");
+    }else{
+        alert("Infelizmente você errou!");
+    }
+}
+
+numeroPensado = randomNumber(numero);
 caixaNumero = document.getElementById("numero");
 botaoAdvinhar = document.getElementById("advinhar");
 botaoAdvinhar.onclick = botaoClicado;
